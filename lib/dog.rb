93de5aad171c62120b1251a2a6ad36842402a904
github username <github email address>
2017-12-14 17:39:16 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Dog
+  attr_reader :name
+  @@all = []
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    dogs = @@all.each {|dog| puts dog.name}
+    # binding.pry
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+end
+
+# pluto = Dog.new("Pluto")
+# fido = Dog.new("Fido")
+# maddy = Dog.new("Maddy")
+#
+# binding.pry
